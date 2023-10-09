63242e1f981a44c16cd71292924043677f9feb49
Ambellofficial <fathiubello1@gmail.com>
2023-10-09 16:55:32 -0700

    Yay, the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabets
+ * Return: (0) success
+ */
+int main(void)
+{
+	char alphas = 'a';
+
+	while (alphas < 'z')
+	{
+		putchar(alphas);
+		alphas++;
+	}
+	putchar('\n');
+	return (0);
+}
