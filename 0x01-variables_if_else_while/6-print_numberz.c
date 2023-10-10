3878788894a218382a1311d2150d991b879e4879
Ambellofficial <fathiubello1@gmail.com>
2023-10-10 06:42:17 -0700

    Prints all single digit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints digits
+ * Return: (0) success
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
