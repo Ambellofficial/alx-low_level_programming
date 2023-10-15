f5cf3e0d32fece2e9424e614709118b788211f86
Ambellofficial <fathiubello1@gmail.com>
2023-10-15 15:12:16 -0700

    checks for alphabets

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -11,13 +11,13 @@
 int _isalpha(int c)
 {
 	char low, up;
-	int letter;
+	int letter = 0;
 
 	for (low = 'a'; low <= 'z'; low++)
 	{
 		for (up = 'A'; up <= 'Z'; up++)
 		{
-			if (low == c || up == 'C')
+			if (low == c || up == c)
 			{
 				letter = 1;
 			}
