718b1ea1348a800bcbf15e2fa2fb2eb13aacf258
Ambellofficial <fathiubello1@gmail.com>
2023-10-30 03:38:36 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,15 +2,12 @@
 /**
  * _abs - computes absolute value of an integer
  * Return: (0)
+ * @n: input
  */
 int _abs(int n)
 {
 	if (n >= 0)
-	{
 		return (n);
-	}
 	else
-	{
-		return (n * = -1);
-	}
+		return (n * -1);
 }
